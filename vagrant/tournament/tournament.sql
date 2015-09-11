b4adcad8441af7b9522206fbbfb86b0a682372f7
rumblefish2494 <rumbelfish2494@gmail.com>
2015-09-10 20:32:39 -0700

    cleaned up comments

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,10 +1,5 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+
 -- create players table with id name wins and matches column
 create table players(id serial, name text, wins integer, matches integer);
 
